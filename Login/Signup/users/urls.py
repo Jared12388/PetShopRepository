2d587ed93a62034f3302120d397fb15931c682ea
William <williamsevenial2003@gmail.com>
2024-09-30 22:16:44 +0800

    Initial commit

---
+++ Login/Signup/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import login_view, signup_view, home
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('login/', login_view, name='login'),
+    path('signup/', signup_view, name='signup'),
+]
