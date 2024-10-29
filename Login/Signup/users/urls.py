dbf02296edb60bd1169fc05260386f90e4726483
Anonimos0703 <williamsevenial2003@gmail.com>
2024-10-29 21:22:48 +0800

    Delete Login/Signup directory

--- Login/Signup/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import login_view, signup_view, home
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('login/', login_view, name='login'),
-    path('signup/', signup_view, name='signup'),
-]
